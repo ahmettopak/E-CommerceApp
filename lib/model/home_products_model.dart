51e6f2fb6f4556b40bd2d3f004dfd99304214d37
Ahmet TOPAK <75997899+ahmettopak@users.noreply.github.com>
2023-03-20 23:39:26 +0300

    Project create

---
+++ lib/model/home_products_model.dart
@@ -0,0 +1,10 @@
+import 'package:ecommerce_app/model/product.dart';
+
+class HomeProductsModel {
+  String categoryTitle;
+  List<Product> products;
+  HomeProductsModel({
+    required this.categoryTitle,
+    required this.products,
+  });
+}
